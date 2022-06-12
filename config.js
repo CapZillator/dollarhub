ef8492ae578cfcd4594d0d876047ec4b3b567bf8
Captain Jack <andruf52@gmail.com>
2022-06-12 12:46:54 +0400

    Config.js updated.

--- config.js
+++ config.js
@@ -1,24 +1,5 @@
 const config = {
     db: {
-      /* don't expose password or any sensitive info, done only for demo */
-      /*
-      connectionLimit : 10,
-      host: "localhost",
-      user: "root",
-      password: "secret",
-      database: "obmennik"
-      connectionLimit : 10,
-      host: "162.241.123.51",
-      user: "dollaymv_kassir",
-      password: "!Aaf141183",
-      database: "dollaymv_obmennik"
-
-      connectionLimit : 10,
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME
-      */
       connectionLimit : 10,
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
